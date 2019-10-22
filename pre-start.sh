034b4ba61ccdfb5cbbd8ddb206dd4a90fba3f05c
Tanvir Rahman <bitbd83@users.noreply.github.com>
2019-10-22 13:16:19 +0600

    update dependencies

--- pre-start.sh
+++ pre-start.sh
@@ -14,3 +14,10 @@ echo "Updating added repository lists..."
 sudo apt update
 echo "Installing tools..."
 sudo apt-get -qq install protobuf-compiler git pkg-config aptitude
+sudo apt-get install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils git cmake libboost-all-dev libgmp3-dev
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:bitcoin/bitcoin
+sudo apt-get update
+sudo apt-get install libdb4.8-dev libdb4.8++-dev
+sudo apt-get install libdb5.3++-dev
+sudo apt-get install libminiupnpc-dev
